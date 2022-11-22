1fbe6504857155f1bcd4e111dc370b8303e226b8
r-q <scrisqiu@hotmail.com>
2022-11-22 23:49:46 +0800

    make basic function

--- generate-font-slice.js
+++ generate-font-slice.js
@@ -1,13 +1,12 @@
-
 let fontAPI = `https://lab.magiconch.com/api/fontmin`;
-const generateFontURL = (name,text)=>{
+const generateFontURL = (name, text) => {
     text += '0';
-    text = text.replace(/\s/g,'');
+    text = text.replace(/\s/g, '');
     text = Array.from(new Set(text)).sort().join('');
 
     console.log(text);
-    
-    if(!text) return requestAnimationFrame(onOver);
+
+    if (!text) return requestAnimationFrame(onOver);
 
     const unicode = str2utf8(text).join();
     const fontURL = `${fontAPI}?name=${name}&unicode=${unicode}&type=woff`;
@@ -15,55 +14,58 @@ const generateFontURL = (name,text)=>{
     return fontURL;
 };
 
-const getFontFromText = (name,text,onOver=_=>{})=>{
-    const fontURL = generateFontURL(name,text);
-    loadFont(name,fontURL,_=>{
+const getFontFromText = (name, text, onOver = _ => {
+}) => {
+    const fontURL = generateFontURL(name, text);
+    loadFont(name, fontURL, _ => {
         onOver(_)
         // style.innerHTML = `html {font-family: a123;}`;
     })
 };
 
-const loadFont = async (fontName,fontURL,callback) => {
-	const fontFace = new FontFace(fontName, `url(${fontURL})`);
-	fontFace.load().then(fontFace => {
-		document.fonts.add(fontFace);
-		callback(fontFace);
-	}).catch(e=>{
+const loadFont = async (fontName, fontURL, callback) => {
+    const fontFace = new FontFace(fontName, `url(${fontURL})`);
+    fontFace.load().then(fontFace => {
+        document.fonts.add(fontFace);
+        callback(fontFace);
+    }).catch(e => {
         // console.log(e);
         callback();
     })
 };
+
 function str2utf8(str) {
-    return str.split('').map(s=>s.charCodeAt(0))
+    return str.split('').map(s => s.charCodeAt(0))
 }
+
 function utf82str(str) {
-    return String.fromCharCode.apply(null,Array.from(str))
+    return String.fromCharCode.apply(null, Array.from(str))
 };
 
-const { readFileSync, writeFileSync } = require('fs');
-const getTextFromHTMLFile = (filename)=>{
-    let html = readFileSync(filename,'utf8');
+const {readFileSync, writeFileSync} = require('fs');
+const getTextFromHTMLFile = (filename) => {
+    let html = readFileSync(filename, 'utf8');
     const texts = [];
 
-    const addTextFromMatch = (all,text)=>{
-        text = text.replace(/<.+?>/g,'');
+    const addTextFromMatch = (all, text) => {
+        text = text.replace(/<.+?>/g, '');
 
         texts.push(text);
         return '';
     }
-    html = html.replace(/<(?:path|rect) id="(.+?)"/g,addTextFromMatch);
-    html = html.replace(/<text .+?>(.+?)<\/text>/g,addTextFromMatch);
-    html = html.replace(/<a .+?>(.+?)<\/a>/g,addTextFromMatch);
-    html = html.replace(/<h2 .+?>(.+?)<\/h2>/g,addTextFromMatch);
-    html = html.replace(/<p>([\s\S]+?)<\/p>/g,addTextFromMatch);
-    html = html.replace(/id="(.+?)"/g,addTextFromMatch);
+    html = html.replace(/<(?:path|rect) id="(.+?)"/g, addTextFromMatch);
+    html = html.replace(/<text .+?>(.+?)<\/text>/g, addTextFromMatch);
+    html = html.replace(/<a .+?>(.+?)<\/a>/g, addTextFromMatch);
+    html = html.replace(/<h2 .+?>(.+?)<\/h2>/g, addTextFromMatch);
+    html = html.replace(/<p>([\s\S]+?)<\/p>/g, addTextFromMatch);
+    html = html.replace(/id="(.+?)"/g, addTextFromMatch);
 
     return texts.join('');
 };
 
 const defaultText = `1234567890:`;
-const text = defaultText + getTextFromHTMLFile('html/index.html') + getTextFromHTMLFile('world.svg');
-const fontURL = generateFontURL(`JiaLiDaYuanJF`,text);
+const text = defaultText + getTextFromHTMLFile('html/index.html') + getTextFromHTMLFile('nkd-ex2.svg');
+const fontURL = generateFontURL(`JiaLiDaYuanJF`, text);
 
 
 // console.log(text);
@@ -72,12 +74,12 @@ const fontURL = generateFontURL(`JiaLiDaYuanJF`,text);
 const axios = require('axios');
 
 
-const downFontFile = async _=>{
-    let r = await axios.get(fontURL,{
+const downFontFile = async _ => {
+    let r = await axios.get(fontURL, {
         responseType: 'arraybuffer'
     });
     // console.log(r.data);
-    writeFileSync('html/slice.woff',r.data,'binary');
+    writeFileSync('html/slice.woff', r.data, 'binary');
 };
 
 downFontFile();
--- html/document.js
+++ html/document.js
@@ -1,39 +1,39 @@
 const doc = document;
 const htmlEl = doc.documentElement;
-const { body, head } = doc;
+const {body, head} = doc;
 const createElement = n => doc.createElement(n);
-const createImage = _=> new Image();
-const addEvent = (el,ev,cb) => el[`on${ev}`] = cb;// 元素.addEventListener(事件,cb);
+const createImage = _ => new Image();
+const addEvent = (el, ev, cb) => el[`on${ev}`] = cb;// 元素.addEventListener(事件,cb);
 const getRect = el => el.getBoundingClientRect();
-const $ = (s,el=doc) => el.querySelector(s);
+const $ = (s, el = doc) => el.querySelector(s);
 const setlevelEl = $('.set-level');
 const setLevelTitleEl = setlevelEl.children[0];
 const countryBoxEl = $('#countrys');
-const closeAll = _=>{
+const closeAll = _ => {
     setLevelStyle.display = '';
 };
 const data = {};
-const getCountryEls = _=>[...countryBoxEl.children];
-const getLevels = _=>getCountryEls().map(el=>el.getAttribute('level')||'0');
+const getCountryEls = _ => [...countryBoxEl.children];
+const getLevels = _ => getCountryEls().map(el => el.getAttribute('level') || '0');
 const localStorageLevelsKey = 'world-ex-levels';
-const saveLevels = _=>{
-    localStorage.setItem(localStorageLevelsKey,getLevels().join(''));
+const saveLevels = _ => {
+    localStorage.setItem(localStorageLevelsKey, getLevels().join(''));
 };
-const gelLevelsAndSet = _=>{
+const gelLevelsAndSet = _ => {
     const levels = localStorage.getItem(localStorageLevelsKey) || '';
-    getCountryEls().forEach((el,i)=>{
-        el.setAttribute('level',levels[i]||'0')
+    getCountryEls().forEach((el, i) => {
+        el.setAttribute('level', levels[i] || '0')
     })
 };
 const svgEl = body.querySelector('svg');
 const setLevelStyle = setlevelEl.style;
 const minMargin = 6;
-addEvent(countryBoxEl,'click', e=>{
+addEvent(countryBoxEl, 'click', e => {
     e.stopPropagation();
 
-    const { target } = e;
+    const {target} = e;
     const targetRect = getRect(target);
-    const { id } = target;
+    const {id} = target;
     data.target = target;
     data.id = id;
 
@@ -43,13 +43,13 @@ addEvent(countryBoxEl,'click', e=>{
     const targetBBox = target.getBBox();
     const svgElBBox = svgEl.getBBox();
     const svgRect = getRect(svgEl);
-    
+
     // console.log(e)
     // console.log(target)
     // console.log(setLevelElRect)
     // console.log(svgRect)
 
-    let left = Math.round(body.scrollLeft + targetRect.left + targetRect.width/2 - setLevelElRect.width/2);
+    let left = Math.round(body.scrollLeft + targetRect.left + targetRect.width / 2 - setLevelElRect.width / 2);
     // left = Math.min(
     //     left,
     //     body.offsetWidth + body.scrollWidth - setLevelElRect.width - minMargin
@@ -59,7 +59,7 @@ addEvent(countryBoxEl,'click', e=>{
     //     minMargin
     // );
 
-    let top = Math.round(body.scrollTop + targetRect.top + targetRect.height/2 - setLevelElRect.height/2);
+    let top = Math.round(body.scrollTop + targetRect.top + targetRect.height / 2 - setLevelElRect.height / 2);
     // top = Math.min(
     //     top,
     //     body.offsetHeight + body.scrollHeight - setLevelElRect.height - minMargin
@@ -78,26 +78,26 @@ addEvent(countryBoxEl,'click', e=>{
     top = targetBBox.y * scalc + svgElBBox.y;
     // console.log(svgEl,targetBBox,svgElBBox)
 
-    left = e.pageX + - setLevelElRect.width/2 + body.scrollLeft;
-    top = e.pageY + - setLevelElRect.height/2 + body.scrollTop + 4;
-    
+    left = e.pageX + -setLevelElRect.width / 2 + body.scrollLeft;
+    top = e.pageY + -setLevelElRect.height / 2 + body.scrollTop + 4;
+
 
     setLevelStyle.left = left + 'px';
     setLevelStyle.top = top + 'px';
 });
-addEvent(doc,'click',closeAll);
+addEvent(doc, 'click', closeAll);
 const scoreEl = $('#score');
-const stat = _=>{
+const stat = _ => {
     const score = getLevels().reduce((all, a) => {
-        return +all + (+a||0);
-      }, 0);
+        return +all + (+a || 0);
+    }, 0);
     scoreEl.innerHTML = `分数: ${score}`;
 }
-addEvent(setlevelEl,'click',e=>{
+addEvent(setlevelEl, 'click', e => {
     e.stopPropagation();
     const level = e.target.getAttribute('data-level');
-    if(!level) return false;
-    data.target.setAttribute('level',level);
+    if (!level) return false;
+    data.target.setAttribute('level', level);
     stat();
     closeAll();
     saveLevels();
@@ -106,28 +106,29 @@ addEvent(setlevelEl,'click',e=>{
 gelLevelsAndSet();
 stat();
 
-const readFileToURL = (blob,cb)=>{
+const readFileToURL = (blob, cb) => {
     const reader = new FileReader();
     reader.onload = e => cb(e.target.result);
     reader.readAsDataURL(blob);
 };
-const getFontDataURL = (url,cb)=>{
-    fetch(url).then(r => r.blob()).then(blob => readFileToURL(blob,cb));
+const getFontDataURL = (url, cb) => {
+    fetch(url).then(r => r.blob()).then(blob => readFileToURL(blob, cb));
 };
-const getFontStyle = (fontName,cb)=>{
-    getFontDataURL(`${fontName}.woff?v={version}`,url => cb(`@font-face{font-family:${fontName};src:url(${url})}`));
+const getFontStyle = (fontName, cb) => {
+    getFontDataURL(`${fontName}.woff?v={version}`, url => cb(`@font-face{font-family:${fontName};src:url(${url})}`));
 };
-getFontStyle('slice',styleText=>{
+getFontStyle('slice', styleText => {
     svgEl.querySelector('style').innerHTML = styleText;
     const styleEl = createElement('style');
     styleEl.innerHTML = styleText;
     head.appendChild(styleEl);
-    setTimeout(_=>htmlEl.removeAttribute('data-loading'),2e3);
+    setTimeout(_ => htmlEl.removeAttribute('data-loading'), 2e3);
 });
 
-const width = 2000;
-const height = 1210;
+const width = 1652;
+const height = 1000;
 const zoom = 1;
+console.log(document.body.clientHeight)
 
 const canvas = createElement('canvas');
 
@@ -136,65 +137,65 @@ canvas.height = width * zoom;
 
 const ctx = canvas.getContext('2d');
 
-const fromXMLCreateImageSrc = text=>{
+const fromXMLCreateImageSrc = text => {
     const blob = new Blob([text], {type: 'image/svg+xml'});
     return URL.createObjectURL(blob);
 };
 const isSNS = /weibo|qq/i.test(navigator.userAgent);
 // alert(navigator.userAgent)
-const saveFile = (link,filename,el = createElement('a'))=>{
-    if(!isSNS){
+const saveFile = (link, filename, el = createElement('a')) => {
+    if (!isSNS) {
         el.download = filename;
     }
     el.href = link;
     el.click();
 };
-const urlToImageEl = (url,cb)=>{
+const urlToImageEl = (url, cb) => {
     const img = createImage();
-    addEvent(img,'load',_=>setTimeout(_=>cb(img),500));
+    addEvent(img, 'load', _ => setTimeout(_ => cb(img), 500));
     img.src = url;
 };
-const log = _=>(createImage()).src = `https://lab.magiconch.com/api/world-ex/log?levels=${getLevels().join('')}`;
+const log = _ => (createImage()).src = `https://lab.magiconch.com/api/world-ex/log?levels=${getLevels().join('')}`;
 
 const outputEl = $('.output');
 const outputImageStyle = outputEl.style;
-const generateName = _=>Math.floor((+new Date()-163e10)/1e4).toString(32);
-const saveImage = _=>{
-    htmlEl.setAttribute('data-running','true');
+const generateName = _ => Math.floor((+new Date() - 163e10) / 1e4).toString(32);
+const saveImage = _ => {
+    htmlEl.setAttribute('data-running', 'true');
 
     const xmlText = `<?xml version="1.0" encoding="utf-8"?><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${width} ${height}" width="${width}px" height="${height}px">${svgEl.innerHTML}</svg>`;
     const dataURL = fromXMLCreateImageSrc(xmlText);
-    urlToImageEl(dataURL,imgEl=>{
+    urlToImageEl(dataURL, imgEl => {
         ctx.fillStyle = '#BAE4FF';
         ctx.fillRect(
-            0,0,
-            width * zoom,width * zoom
+            0, 0,
+            width * zoom, width * zoom
         );
         ctx.drawImage(
             imgEl,
-            0,0,
-            width,height,
+            0, 0,
+            width, height,
             0, (width - height) * zoom / 2,
             width * zoom, height * zoom
         );
-        canvas.toBlob(blob=>{
+        canvas.toBlob(blob => {
             const url = URL.createObjectURL(blob);
             outputEl.querySelector('img').src = url;
             outputImageStyle.display = '';
 
-            setTimeout(_=>{
-                saveFile(url,`[神奇海螺][全球制霸]${generateName()}.png`);
+            setTimeout(_ => {
+                saveFile(url, `[神奇海螺][全球制霸]${generateName()}.png`);
                 htmlEl.removeAttribute('data-running');
-            },50)
-            
+            }, 50)
 
-        },'image/png');
+
+        }, 'image/png');
     });
     log();
 };
 
-addEvent($('.save-btn'),'click',saveImage);
+addEvent($('.save-btn'), 'click', saveImage);
 
-addEvent($('a',outputEl),'click',_=>{
+addEvent($('a', outputEl), 'click', _ => {
     outputImageStyle.display = 'none'
 });
